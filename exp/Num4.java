f820ae0b09ae7e3c801e9c5950959ef7859b98bd
xiaomineu <zhangminneu@sina.com>
2019-04-03 01:23:38 +0800

    网易笔试update

--- exp/Num4.java
+++ exp/Num4.java
@@ -1,68 +1,122 @@
-//package exp;
-//
-//import java.util.ArrayList;
-//import java.util.Scanner;
-//
-//public class Num4 {
-//    public static void main(String[] args)
-//    {
-//        Scanner in=new Scanner(System.in);
-//        int n=in.nextInt();
-//        int [] a=new int[n];
-//        int [][] b=new int[n][2];
-//        int []flag=new int[n];
-//        int left=0;
-//        int right=n-1;
-//        for(int i=0;i<n;i++)
-//        {
-//            a[i]=in.nextInt();
-//            b[i][0]=a[i];
-//            b[i][1]=i;
-//            flag[i]=0;
-//        }
-//        int ans=0;
-//        if(a[0]==1&&n>1)
-//        {
-//            ans+=a[1];
-//            flag[0]=1;
-//            left=1;
-//        }
-//        if(a[n-1]==1&&n>1)
-//        {
-//            ans+=a[n-2];
-//            flag[n-1]=1;
-//            right=n-2;
-//        }
-//        slowsort(b,n);
-//        for(int i=0;i<=n;i++)
-//        {
-//            int index=b[i][1];
-//            if(flag[index]==1)
-//            {
-//                continue;
-//            }
-//            int leftindex=findleft(left,index,flag);
-//            int rightindex=findright(index,flag,right);
-//
-//        }
-//
-//    }
-//
-//    private static void slowsort(int[][] b,int n)
-//    {
-//        for(int i=0;i<n;i++)
-//        {
-//            for(int j=i-1;j>-1;j--)
-//            {
-//                if(b[i][0]<b[j][0])
-//                {
-//                    int x=b[i][0];
-//                    b[i][0]=b[j][0];
-//                    b[j][0]=x;
-//                    int y=b[i][1];
-//                    b[i][1]=b[j][1];
-//                }
-//            }
-//        }
-//    }
-//}
+package exp;
+
+import java.util.Scanner;
+
+public class Num4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        int[][] b = new int[n][2];
+        int[] flag = new int[n];
+        int left = 0;
+        int right = n - 1;
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+            b[i][0] = a[i];
+            b[i][1] = i;//记录当前元素位置;
+        }
+        slowsort(b,n);
+        int ans=0;
+        int count=1;
+        while(a[left]==1&&n>1&&left<n-1)
+        {
+            while(a[left]==a[left+1])
+            {
+                flag[left]=1;
+                left++;
+                count++;
+            }
+            ans+=a[left+1]*count;
+            flag[left]=1;
+            left++;
+        }
+        count=1;
+        while(a[right]==1&&n>1&&right>-1)
+        {
+            while(a[right]==a[right-1])
+            {
+                flag[right]=1;
+                right--;
+                count++;
+            }
+            ans+=a[n-2]*count;
+            flag[right]=1;
+            right--;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            int index=b[i][1];
+            if(flag[index]==1||index==left||index==right)
+            {
+                continue;
+            }
+            int leftIndex=Leftposition(left,index,flag);
+            int rightIndex=Rightposition(right,index,flag);
+            int add=b[i][0];
+            if(leftIndex>=left)
+            {
+                add*=a[leftIndex];
+            }
+            if(rightIndex<=right)
+            {
+                add*=a[rightIndex];
+            }
+            flag[index]=1;
+            ans+=add;
+        }
+        ans+=a[left]*a[right];
+        if(a[left]>a[right])
+        {
+            ans+=a[left];
+        }
+        else
+            ans+=a[right];
+
+
+        System.out.println(ans);
+    }
+
+    private static int Leftposition(int left,int position,int []flag)
+    {
+        int x=position-1;
+        while(x>left&&flag[x]==1)
+        {
+            x--;
+        }
+        return x;
+    }
+
+
+    private static int Rightposition(int right,int position,int []flag)
+    {
+        int x=position+1;
+        while(x<right&&flag[x]==1)
+        {
+            x++;
+        }
+        return x;
+    }
+
+
+
+    private static void slowsort(int[][] b,int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n-1;j++)
+            {
+                if(b[j][0]>b[j+1][0])
+                {
+                    int x=b[j][0];
+                    b[j][0]=b[j+1][0];
+                    b[j+1][0]=x;
+                    int y=b[j][1];
+                    b[j][1]=b[j+1][1];
+                    b[j+1][1]=y;
+                }
+            }
+        }
+    }
+}
